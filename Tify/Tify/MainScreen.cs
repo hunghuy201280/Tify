8c61c0b137e580db778116d12f9777e92076bffb
Huy <19521638@gm.uit.edu.vn>
2020-11-09 15:15:28 +0700

    update

--- Tify/Tify/MainScreen.cs
+++ Tify/Tify/MainScreen.cs
@@ -1,18 +1,6 @@
-﻿using NAudio.Wave;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Media;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using WMPLib;
 
@@ -20,66 +8,62 @@ namespace Tify
 {
     public partial class MainScreen : Form
     {
-       
         public MainScreen()
         {
             InitializeComponent();
-           
         }
 
-
         #region Load form
+
         private void MainScreen_Load(object sender, EventArgs e)
         {
             openChildForm(new Home(this));
 
-
             volume_trackBar.Anchor = AnchorStyles.Right;
 
-
-            //Hide  ScrollBar cho menu_panel
+            //Hide  ScrollBar so menu_panel
             hideScrollBar(menu_panel, "panel");
 
             this.DoubleBuffered = true;
-
 
             //demo
 
             soundPlayer.URL = @"https://data25.chiasenhac.com/download2/2126/1/2125711-e9320e2c/128/Giau%20Vi%20Ban_%20Sang%20Vi%20Vo%20-%20RPT%20MCK.mp3";
             time = soundPlayer.controls.currentPosition;
-
         }
-        #endregion
+
+        #endregion Load form
 
         #region Đổi icon khi nhấn vào nút play/pause
-        bool isPause = true;
-        WindowsMediaPlayer soundPlayer = new WindowsMediaPlayer();
-        double time;
+
+        private bool isPause = true;
+        private WindowsMediaPlayer soundPlayer = new WindowsMediaPlayer();
+        private double time;
+
         private void pause_button_Click(object sender, EventArgs e)
         {
-
-            
-
-                if (isPause)
-                {
-                    pause_button.BackgroundImage = byteArrayToImage(FileStore.Resource.play);
-                    soundPlayer.controls.currentPosition = time;
-                    soundPlayer.controls.play();
-                }
-                else
-                {
-                    pause_button.BackgroundImage = byteArrayToImage(FileStore.Resource.pause);
-                    time = soundPlayer.controls.currentPosition;
-                    soundPlayer.controls.pause();
-                }
-                isPause = !isPause;
+            if (isPause)
+            {
+                pause_button.BackgroundImage = byteArrayToImage(FileStore.Resource.play);
+                soundPlayer.controls.currentPosition = time;
+                soundPlayer.controls.play();
+            }
+            else
+            {
+                pause_button.BackgroundImage = byteArrayToImage(FileStore.Resource.pause);
+                time = soundPlayer.controls.currentPosition;
+                soundPlayer.controls.pause();
+            }
+            isPause = !isPause;
         }
-        #endregion
+
+        #endregion Đổi icon khi nhấn vào nút play/pause
 
         #region Đổi icon khi nhấn vào nút âm lượng
-        bool isMute = false;
 
-        public object Cp { get; private set; }
+        private bool isMute = false;
+
+     
 
         private void volume_button_Click(object sender, EventArgs e)
         {
@@ -89,14 +73,14 @@ namespace Tify
                 volume_button.BackgroundImage = byteArrayToImage(FileStore.Resource.mute);
             isMute = !isMute;
         }
-        #endregion
 
-
+        #endregion Đổi icon khi nhấn vào nút âm lượng
 
         #region event khi Form size đổi
+
         private void Form1_SizeChanged(object sender, EventArgs e)
         {
-            //Chỉnh lại vị trí của 3 dòng title,artist,playingFrom và ẩn hoặc hiện songImg 
+            //Chỉnh lại vị trí của 3 dòng title,artist,playingFrom và ẩn hoặc hiện songImg
             if (this.Size.Width <= 975)
             {
                 songImg_pictureBox.Hide();
@@ -104,11 +88,9 @@ namespace Tify
                 title_label.Location = new Point(15, title_label.Location.Y);
                 artist_label.Location = new Point(15, artist_label.Location.Y);
                 playingFrom_label.Location = new Point(15, playingFrom_label.Location.Y);
-
             }
             else
             {
-
                 songImg_pictureBox.Show();
                 //x=104
                 title_label.Location = new Point(104, title_label.Location.Y);
@@ -126,38 +108,32 @@ namespace Tify
                 play_pause_panel.Top = (player_panel.ClientSize.Height - play_pause_panel.Height) / 2;
             }
 
-
-            //Chỉnh lại vị trí của menu bar bên trái 
+            //Chỉnh lại vị trí của menu bar bên trái
             if (this.Size.Width >= 860)
             {
                 changeMenuButtonStyle(false);
-
             }
             else
             {
-
                 changeMenuButtonStyle(true);
-               
             }
-           
-            
-            
-
         }
-        #endregion
+
+        #endregion event khi Form size đổi
 
         #region Đổi màu icon và chữ khi click vào 1 menu button, mở child Form
+
         private void menu_button_Click(object sender, EventArgs e)
         {
             Button btt = sender as Button;
-            if (btt.Image!=null)
+            if (btt.Image != null)
             {
-                if(btt.ImageIndex%2==0)
-                btt.ImageIndex += 1;
+                if (btt.ImageIndex % 2 == 0)
+                    btt.ImageIndex += 1;
             }
             btt.ForeColor = Color.FromArgb(0, 255, 255);
             Button temp;
-            foreach(var item in menu_panel.Controls)
+            foreach (var item in menu_panel.Controls)
             {
                 if (item is Button && item != btt)
                 {
@@ -172,26 +148,28 @@ namespace Tify
                 }
             }
 
-
             //Mở childForm
             if (sender == home_button)
                 openChildForm(new Home(this));
             else
                 MessageBox.Show("Chuaco");
         }
-        #endregion
-        
+
+        #endregion Đổi màu icon và chữ khi click vào 1 menu button, mở child Form
 
         #region Chuyển dạng byte[] trong file resources thành image
+
         public Image byteArrayToImage(byte[] byteArrayIn)
         {
             MemoryStream ms = new MemoryStream(byteArrayIn);
             Image returnImage = Image.FromStream(ms);
             return returnImage;
         }
-        #endregion
+
+        #endregion Chuyển dạng byte[] trong file resources thành image
 
         #region Phóng to & thu nhỏ menu panel khi di chuột vào
+
         private void menu_panel_MouseHover(object sender, EventArgs e)
         {
             //240 65
@@ -210,13 +188,14 @@ namespace Tify
             if (menu_panel.Width == 240 && this.Size.Width < 860)
                 changeMenuButtonStyle(true);
         }
-        #endregion
+
+        #endregion Phóng to & thu nhỏ menu panel khi di chuột vào
 
         #region Đổi size của menu bên trái
+
         //Đổi kích thước, style của button trong menu bên trái
         private void changeMenuButtonStyle(bool change)
         {
-            
             if (!change)
             {
                 myMix_button.Text = myMix_button.Tag.ToString();
@@ -230,7 +209,6 @@ namespace Tify
                 home_pictureBox.Hide();
 
                 menu_panel.Size = new Size(240, menu_panel.Size.Height);
-
 
                 myCollection_label.Text = myCollection_label.Tag.ToString();
                 myPlaylist_label.Text = myPlaylist_label.Tag.ToString();
@@ -253,17 +231,18 @@ namespace Tify
             }
         }
 
-        #endregion
+        #endregion Đổi size của menu bên trái
 
         #region Mở childForm
 
         private Form activeForm = null;
+
         private void openChildForm(Form childForm)
         {
             if (activeForm != null)
                 activeForm.Close();
             activeForm = childForm;
-            
+
             childForm.TopLevel = false;
             childForm.FormBorderStyle = FormBorderStyle.None;
             childForm.Dock = DockStyle.Fill;
@@ -271,17 +250,15 @@ namespace Tify
 
             childForm.BringToFront();
             childForm.Show();
-
-            
         }
 
-        #endregion
+        #endregion Mở childForm
 
         #region hide scrollbar for control
 
-        static public void hideScrollBar(Control needHide,string type)
+        static public void hideScrollBar(Control needHide, string type)
         {
-            if(type=="flow")
+            if (type == "flow")
             {
                 FlowLayoutPanel flowpanel = needHide as FlowLayoutPanel;
                 flowpanel.AutoScroll = false;
@@ -289,13 +266,12 @@ namespace Tify
                 flowpanel.AutoScroll = false;
                 flowpanel.VerticalScroll.Visible = false;
 
-
                 flowpanel.VerticalScroll.Maximum = 0;
                 flowpanel.AutoScroll = false;
                 flowpanel.HorizontalScroll.Visible = false;
                 flowpanel.AutoScroll = true;
             }
-            else if(type=="panel")
+            else if (type == "panel")
             {
                 Panel panel = needHide as Panel;
                 panel.AutoScroll = false;
@@ -303,24 +279,19 @@ namespace Tify
                 panel.AutoScroll = false;
                 panel.VerticalScroll.Visible = false;
 
-
                 panel.VerticalScroll.Maximum = 0;
                 panel.AutoScroll = false;
                 panel.HorizontalScroll.Visible = false;
                 panel.AutoScroll = true;
             }
-
         }
-        #endregion
 
+        #endregion hide scrollbar for control
 
         #region test
+
         //test
 
-        #endregion
+        #endregion test
     }
-
-
-
-
 }
