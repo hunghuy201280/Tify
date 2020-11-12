55a576acae870ff551936ee8621066fdd097a878
khoa le <kevinle38@gmail.com>
2020-11-12 21:41:37 +0700

    update createplaylist: count word // createplaylist :done

--- Tify/Tify/CreatePlayList.cs
+++ Tify/Tify/CreatePlayList.cs
@@ -15,9 +15,29 @@ namespace Tify
         public CreatePlayList()
         {
             InitializeComponent();
-            Text_PlayList_Count.Text = "0/500";
+            Text_PlayList_Count.Text = Description_TextBox.Text.Length.ToString() +"/ 500" ;
 
 
         }
+        int count = 0;
+        private void Description_TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            /* Text_PlayList_Count.Text = Description_TextBox.Text.Length.ToString() + "/ 500";*/
+            if (e.KeyChar == '\b')
+            {
+                count--;
+            }
+            else if (count >= 500)
+            {
+                e.Handled = true;
+            }
+            else
+                count++;
+            if (count <= 0)
+                count = 0;
+            Text_PlayList_Count.Text = count.ToString() + "/ 500";
+        }
+
     }
 }
