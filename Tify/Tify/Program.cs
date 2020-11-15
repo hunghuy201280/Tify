ad2ffe931a40872211177b4a841f44a45f123c2c
khoa le <kevinle38@gmail.com>
2020-11-15 16:46:10 +0700

    ADD Artist when 1st create & ADD Register [GUI]

--- Tify/Tify/Program.cs
+++ Tify/Tify/Program.cs
@@ -16,7 +16,7 @@ namespace Tify
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Register());
+            Application.Run(new Artist_NewCreate());
         }
     }
 }
