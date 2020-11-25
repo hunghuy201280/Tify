1746e7ea92367ebb2ab259a890cbabd4fd16eb58
Huy <19521638@gm.uit.edu.vn>
2020-11-25 20:56:34 +0700

    update

--- SQLQueryForTify.sql
+++ SQLQueryForTify.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Track
+﻿CREATE TABLE Track
 (
 	trackID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	trackGenre nvarchar(50),
@@ -35,3 +35,172 @@ add constraint fk_artistID foreign key (artistID) references Artist(artistID);
 
 Delete  from Artist where artistID='@id';
 select * from Artist;
+
+insert into Album (albumTitle,albumYear,albumLink)
+values ('1',2,'3')
+
+ALTER TABLE Account DROP COLUMN userID
+ALTER TABLE Account ADD userID INT IDENTITY(1,1)
+
+ALTER TABLE Account 
+Add constraint pk_account primary key (userID)
+
+
+ALTER TABLE Album DROP COLUMN albumID
+ALTER TABLE Album ADD albumID INT IDENTITY(1,1)
+
+ALTER TABLE Album 
+Add constraint pk_album primary key (albumID)
+
+
+
+ALTER TABLE AlbumHasTrack 
+Add constraint pk_albumHasTrack primary key (albumID,trackID)
+
+
+
+ALTER TABLE Artist DROP COLUMN csnArtistID
+ALTER TABLE Artist ADD artistID nvarchar(200) 
+
+ALTER TABLE Artist 
+Add constraint pk_artistTable primary key (artistID)
+
+
+UPDATE Artist 
+SET artistID = artistID+1
+where artistID=0;
+
+
+ALTER TABLE Artist
+ALTER COLUMN artistID nvarchar(200) NOT NULL;
+
+ALTER TABLE Artist
+add  artistID
+
+ALTER TABLE Artist
+drop column artistID 
+
+delete  from Artist where artistName=N'Camkytiwa'
+
+
+select * from Artist where artistName=N'Sơn Tùng M-TP'
+
+delete from Album
+select * from Album
+
+ALTER TABLE Track DROP COLUMN trackID
+ALTER TABLE Track ADD trackID INT IDENTITY(1,1)
+
+ALTER TABLE Track 
+Add constraint pk_track primary key (trackID)
+
+Alter table MyMix
+add constraint pk_MyMix primary key (myMixID)
+
+Alter table MyMix
+drop pk_MyMix
+
+
+Alter table MyMix
+drop column myMixTitle
+Alter table MyMix
+add myMixTitle nvarchar(100) not null
+
+alter table MyMixHasTrack
+add constraint pk_MyMixHasTrack primary key(myMixID,trackID)
+
+alter table Playlist
+add constraint pk_Playlist primary key (playlistID)
+
+alter table UserFollowArtist
+add constraint pk_UserFollowArtist primary key (userID,artistID)
+
+alter table UserLikeAlbum
+add constraint pk_UserLikeAlbum primary key (userID,albumID)
+
+alter table UserLikePlaylist
+add constraint pk_UserLikePlaylist primary key (userID,playlistID)
+
+
+alter table UserLikeTrack
+add constraint pk_UserLikeTrack primary key (userID,trackID)
+
+
+alter table AlbumHasTrack
+add constraint fk_trackID_AlbumHasTrack foreign key (trackID) references Track(trackID)
+
+alter table AlbumHasTrack
+add constraint fk_albumID_AlbumHasTrack foreign key (albumID) references Album(albumID)
+
+alter table Artist
+add constraint fk_albumID_Artist foreign key (albumID) references Album(albumID)
+
+alter table Artist
+add constraint fk_trackID_Artist foreign key (trackID) references Track(trackID)
+
+alter table MyMixHasTrack
+add constraint fk_myMixID_MyMixHasTrack foreign key (myMixID) references MyMix(myMixID)
+
+alter table MyMixHasTrack
+add constraint fk_trackID_MyMixHasTrack foreign key (trackID) references Track(trackID)
+
+create table PlaylistHasTrack
+(
+	trackID int not null foreign key references Track(trackID) ,
+	playlistID int not null foreign key references Playlist(playlistID),
+	primary key(trackID,playlistID)
+)
+
+alter table Playlist 
+drop column trackID
+
+alter table UserFollowArtist
+add constraint fk_userID_UserFollowArtist foreign key (userID) references Account(userID)
+
+alter table UserFollowArtist
+add constraint fk_artistID_UserFollowArtist foreign key (artistID) references Artist(artistID)
+
+alter table UserLikeAlbum
+add constraint fk_userID_UserLikeAlbum foreign key (userID) references Account(userID)
+
+alter table UserLikeAlbum
+add constraint fk_albumID_UserLikeAlbum foreign key (albumID) references Album(albumID)
+
+alter table UserHasPlaylist
+add constraint fk_userID_UserHasPlaylist foreign key (userID) references Account(userID)
+
+alter table UserHasPlaylist
+add constraint fk_playlistID_UserHasPlaylist foreign key (playlistID) references Playlist(playlistID)
+
+alter table UserLikeTrack
+add constraint fk_userID_UserLikeTrack foreign key (userID) references Account(userID)
+
+alter table UserLikeTrack
+add constraint fk_trackID_UserLikeTrack foreign key (trackID) references Track(trackID)
+
+select* from Artist
+
+
+alter table Track
+drop column trackGenre
+
+insert into Track values('1','2')
+select * from Track
+
+
+
+
+create table ArtistHasTrack
+(
+	artistID nvarchar(200) not null references Artist(artistID),
+	trackID int not null references Track(trackID),
+	primary key(artistID,trackID)
+)
+
+
+select* from Track
+select * from Artist
+
+insert into Track values('Big City Boi','https://vi.chiasenhac.vn/mp3/binz-touliver/big-city-boi-tsvwzbwrq91q9a.html')
+ALTER TABLE Track
+ADD CONSTRAINT UN_Track UNIQUE (trackTitle,trackLink);
