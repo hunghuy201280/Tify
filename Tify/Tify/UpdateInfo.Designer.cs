eb78da2853db2c3493f84a3ae91d78f45c1a70fd
khoale38 <kevinle38@gmail.com>
2020-12-16 23:30:12 +0700

    GUI UPDATE INFO

---
+++ Tify/Tify/UpdateInfo.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Tify
+{
+    partial class UpdateInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateInfo));
+            this.Label_name = new System.Windows.Forms.Label();
+            this.label_password = new System.Windows.Forms.Label();
+            this.label_DOB = new System.Windows.Forms.Label();
+            this.comboBox_day = new System.Windows.Forms.ComboBox();
+            this.comboBox_month = new System.Windows.Forms.ComboBox();
+            this.txtbox_name = new System.Windows.Forms.TextBox();
+            this.textBox_pwd = new System.Windows.Forms.TextBox();
+            this.textBox_year = new System.Windows.Forms.TextBox();
+            this.label_phonenumber = new System.Windows.Forms.Label();
+            this.textBox_Phone = new System.Windows.Forms.TextBox();
+            this.button_save = new System.Windows.Forms.Button();
+            this.button_quit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label_name
+            // 
+            this.Label_name.AutoSize = true;
+            this.Label_name.Font = new System.Drawing.Font("Nationale Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_name.ForeColor = System.Drawing.Color.White;
+            this.Label_name.Location = new System.Drawing.Point(175, 173);
+            this.Label_name.Name = "Label_name";
+            this.Label_name.Size = new System.Drawing.Size(78, 25);
+            this.Label_name.TabIndex = 0;
+            this.Label_name.Text = "Name: ";
+            // 
+            // label_password
+            // 
+            this.label_password.AutoSize = true;
+            this.label_password.Font = new System.Drawing.Font("Nationale Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_password.ForeColor = System.Drawing.Color.White;
+            this.label_password.Location = new System.Drawing.Point(141, 235);
+            this.label_password.Name = "label_password";
+            this.label_password.Size = new System.Drawing.Size(112, 25);
+            this.label_password.TabIndex = 1;
+            this.label_password.Text = "Password: ";
+            // 
+            // label_DOB
+            // 
+            this.label_DOB.AutoSize = true;
+            this.label_DOB.Font = new System.Drawing.Font("Nationale Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_DOB.ForeColor = System.Drawing.Color.White;
+            this.label_DOB.Location = new System.Drawing.Point(121, 298);
+            this.label_DOB.Name = "label_DOB";
+            this.label_DOB.Size = new System.Drawing.Size(132, 25);
+            this.label_DOB.TabIndex = 2;
+            this.label_DOB.Text = "Date of birth:";
+            // 
+            // comboBox_day
+            // 
+            this.comboBox_day.BackColor = System.Drawing.Color.Gray;
+            this.comboBox_day.DisplayMember = "1";
+            this.comboBox_day.Enabled = false;
+            this.comboBox_day.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox_day.Font = new System.Drawing.Font("Nationale Light", 9F);
+            this.comboBox_day.FormattingEnabled = true;
+            this.comboBox_day.Location = new System.Drawing.Point(259, 298);
+            this.comboBox_day.Name = "comboBox_day";
+            this.comboBox_day.Size = new System.Drawing.Size(93, 27);
+            this.comboBox_day.TabIndex = 3;
+            // 
+            // comboBox_month
+            // 
+            this.comboBox_month.BackColor = System.Drawing.Color.Gray;
+            this.comboBox_month.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_month.Enabled = false;
+            this.comboBox_month.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox_month.Font = new System.Drawing.Font("Nationale Light", 9F);
+            this.comboBox_month.ForeColor = System.Drawing.Color.White;
+            this.comboBox_month.FormattingEnabled = true;
+            this.comboBox_month.Location = new System.Drawing.Point(376, 298);
+            this.comboBox_month.Name = "comboBox_month";
+            this.comboBox_month.Size = new System.Drawing.Size(142, 27);
+            this.comboBox_month.TabIndex = 4;
+            this.comboBox_month.TextChanged += new System.EventHandler(this.comboBox_month_TextChanged);
+            // 
+            // txtbox_name
+            // 
+            this.txtbox_name.BackColor = System.Drawing.Color.Black;
+            this.txtbox_name.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtbox_name.Font = new System.Drawing.Font("Nationale Light", 10.2F);
+            this.txtbox_name.ForeColor = System.Drawing.Color.White;
+            this.txtbox_name.Location = new System.Drawing.Point(249, 178);
+            this.txtbox_name.Name = "txtbox_name";
+            this.txtbox_name.Size = new System.Drawing.Size(388, 22);
+            this.txtbox_name.TabIndex = 5;
+            // 
+            // textBox_pwd
+            // 
+            this.textBox_pwd.BackColor = System.Drawing.Color.Black;
+            this.textBox_pwd.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_pwd.Font = new System.Drawing.Font("Nationale Light", 10.2F);
+            this.textBox_pwd.ForeColor = System.Drawing.Color.White;
+            this.textBox_pwd.Location = new System.Drawing.Point(249, 240);
+            this.textBox_pwd.Name = "textBox_pwd";
+            this.textBox_pwd.Size = new System.Drawing.Size(388, 22);
+            this.textBox_pwd.TabIndex = 6;
+            // 
+            // textBox_year
+            // 
+            this.textBox_year.BackColor = System.Drawing.Color.Gray;
+            this.textBox_year.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_year.Font = new System.Drawing.Font("Nationale Light", 11F);
+            this.textBox_year.ForeColor = System.Drawing.Color.White;
+            this.textBox_year.Location = new System.Drawing.Point(549, 299);
+            this.textBox_year.Name = "textBox_year";
+            this.textBox_year.Size = new System.Drawing.Size(88, 23);
+            this.textBox_year.TabIndex = 7;
+            this.textBox_year.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_year_KeyPress);
+            // 
+            // label_phonenumber
+            // 
+            this.label_phonenumber.AutoSize = true;
+            this.label_phonenumber.Font = new System.Drawing.Font("Nationale Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_phonenumber.ForeColor = System.Drawing.Color.White;
+            this.label_phonenumber.Location = new System.Drawing.Point(121, 362);
+            this.label_phonenumber.Name = "label_phonenumber";
+            this.label_phonenumber.Size = new System.Drawing.Size(159, 25);
+            this.label_phonenumber.TabIndex = 8;
+            this.label_phonenumber.Text = "Phone Number:";
+            // 
+            // textBox_Phone
+            // 
+            this.textBox_Phone.BackColor = System.Drawing.Color.Black;
+            this.textBox_Phone.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_Phone.Font = new System.Drawing.Font("Nationale Light", 10.2F);
+            this.textBox_Phone.ForeColor = System.Drawing.Color.White;
+            this.textBox_Phone.Location = new System.Drawing.Point(286, 367);
+            this.textBox_Phone.Name = "textBox_Phone";
+            this.textBox_Phone.Size = new System.Drawing.Size(351, 22);
+            this.textBox_Phone.TabIndex = 9;
+            this.textBox_Phone.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_Phone_KeyPress);
+            // 
+            // button_save
+            // 
+            this.button_save.BackColor = System.Drawing.Color.Gray;
+            this.button_save.FlatAppearance.BorderSize = 0;
+            this.button_save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_save.Font = new System.Drawing.Font("Nationale", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_save.ForeColor = System.Drawing.Color.White;
+            this.button_save.Location = new System.Drawing.Point(524, 482);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(120, 53);
+            this.button_save.TabIndex = 10;
+            this.button_save.Text = "Save";
+            this.button_save.UseVisualStyleBackColor = false;
+            // 
+            // button_quit
+            // 
+            this.button_quit.BackColor = System.Drawing.Color.Gray;
+            this.button_quit.FlatAppearance.BorderSize = 0;
+            this.button_quit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_quit.Font = new System.Drawing.Font("Nationale", 13.8F, System.Drawing.FontStyle.Bold);
+            this.button_quit.ForeColor = System.Drawing.Color.White;
+            this.button_quit.Location = new System.Drawing.Point(657, 482);
+            this.button_quit.Name = "button_quit";
+            this.button_quit.Size = new System.Drawing.Size(120, 53);
+            this.button_quit.TabIndex = 11;
+            this.button_quit.Text = "Quit";
+            this.button_quit.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(322, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(249, 206);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(388, 1);
+            this.panel1.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(249, 268);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(388, 1);
+            this.panel2.TabIndex = 15;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(249, 328);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(388, 1);
+            this.panel3.TabIndex = 16;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(286, 395);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(351, 1);
+            this.panel4.TabIndex = 17;
+            // 
+            // UpdateInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 562);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_quit);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.textBox_Phone);
+            this.Controls.Add(this.label_phonenumber);
+            this.Controls.Add(this.textBox_year);
+            this.Controls.Add(this.textBox_pwd);
+            this.Controls.Add(this.txtbox_name);
+            this.Controls.Add(this.comboBox_month);
+            this.Controls.Add(this.comboBox_day);
+            this.Controls.Add(this.label_DOB);
+            this.Controls.Add(this.label_password);
+            this.Controls.Add(this.Label_name);
+            this.Name = "UpdateInfo";
+            this.Text = "UpdateInfo";
+            this.Load += new System.EventHandler(this.UpdateInfo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label_name;
+        private System.Windows.Forms.Label label_password;
+        private System.Windows.Forms.Label label_DOB;
+        private System.Windows.Forms.ComboBox comboBox_day;
+        private System.Windows.Forms.ComboBox comboBox_month;
+        private System.Windows.Forms.TextBox txtbox_name;
+        private System.Windows.Forms.TextBox textBox_pwd;
+        private System.Windows.Forms.TextBox textBox_year;
+        private System.Windows.Forms.Label label_phonenumber;
+        private System.Windows.Forms.TextBox textBox_Phone;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.Button button_quit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
---
+++ Tify/Tify/UpdateInfo.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tify
+{
+    public partial class UpdateInfo : Form
+    {
+        public UpdateInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateInfo_Load(object sender, EventArgs e)
+        {
+            textBox_year.GotFocus += TextBox_year_GotFocus;
+            textBox_year.LostFocus += TextBox_year_LostFocus;
+            textBox_year.Text = "Year";
+            addmonth();
+        }
+
+        private void TextBox_year_LostFocus(object sender, EventArgs e)
+        {
+            
+            if (string.IsNullOrWhiteSpace(textBox_year.Text))
+            {
+                textBox_year.Text = "Year";
+               
+                comboBox_month.Enabled = false;
+
+            }
+            else
+            {
+                if (int.Parse(textBox_year.Text) >= DateTime.Now.Year)
+                {
+                    textBox_year.Text = DateTime.Now.Year.ToString();
+                }
+                if (!string.IsNullOrWhiteSpace(textBox_year.Text))
+                {
+                    
+                    comboBox_month.Enabled = true;
+
+                }
+            }
+        }
+
+        private void TextBox_year_GotFocus(object sender, EventArgs e)
+        {
+            if (textBox_year.Text == "Year")
+                textBox_year.Text = "";
+        
+
+        }
+
+        private void textBox_year_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            textBox_year.MaxLength = 4;
+           
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            
+
+        }
+        void addmonth()
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                comboBox_month.Items.Add(i);
+            }
+            
+        }
+
+        private void comboBox_month_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox_month.Text != "")
+                comboBox_day.Enabled = true;
+            addday();
+        }
+        void addday()
+        {
+           int days= DateTime.DaysInMonth(int.Parse(textBox_year.Text), int.Parse(comboBox_month.Text));
+            for (int i = 1; i <= days; i++)
+            {
+                comboBox_day.Items.Add(i);
+            }
+        }
+
+        private void textBox_Phone_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            textBox_Phone.MaxLength = 10;
+           
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            
+        }
+    }
+}
