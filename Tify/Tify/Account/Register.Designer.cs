5158c4a144ef0de7a1a65ef98b0731db91bdba72
Huy <19521638@gm.uit.edu.vn>
2021-01-03 15:26:05 +0700

    update

--- Tify/Tify/Account/Register.Designer.cs
+++ Tify/Tify/Account/Register.Designer.cs
@@ -240,6 +240,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Nationale Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Register";
